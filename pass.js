83163e36361ee75410aa314a9d0016d55743d80b
Rahul Aware <rahul.aware@gmail.com>
2017-09-27 00:45:27 -0700

    added digest to pass.js

--- pass.js
+++ pass.js
@@ -30,13 +30,13 @@ var iterations = 12000;
 
 exports.hash = function (pwd, salt, fn) {
     if (3 == arguments.length) {
-        crypto.pbkdf2(pwd, salt, iterations, len, fn);
+        crypto.pbkdf2(pwd, salt, iterations, len, 'sha512', fn);
     } else {
         fn = salt;
         crypto.randomBytes(len, function(err, salt){
             if (err) return fn(err);
             salt = salt.toString('base64');
-            crypto.pbkdf2(pwd, salt, iterations, len,'sha512',function(err, hash){
+            crypto.pbkdf2(pwd, salt, iterations, len, 'sha512', function(err, hash){
                 if (err) return fn(err);
                 fn(null, salt, hash);
             });
